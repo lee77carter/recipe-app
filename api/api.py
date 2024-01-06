e7a6598c75903923ae9d0f06d99461533a97349a
lee_c <gitleelee@gmail.com>
2024-01-06 11:28:19 -0800

    Created PUT & DELETE endpoints.

--- api/api.py
+++ api/api.py
@@ -74,7 +74,56 @@ def add_recipe():
     }
     
     return jsonify({'message': 'Recipe added successfully', 'recipe': new_recipe_data})
+
+# PUT Route
+@app.route('/api/recipes<int:recipe_id>', methods=['PUT'])
+def update_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    # validate if recipe exist
+    if not recipe:
+        return jsonify({'error': 'Recipe not found'}), 404
     
+    data = request.get_json()
+    
+   # Validate the incoming JSON data for required fields
+    required_fields = ['title', 'ingredients','instructions', 'servings', 'description', 'image_url']
+    
+    for field in required_fields:
+        if field not in data or data[field] == "":
+            return jsonify({'error': f"Missing required field: '{field}'"}), 400
+    
+    recipe.title = data['title']
+    recipe.ingredients = data['ingredients']
+    recipe.instructions = data['instructions']
+    recipe.servings = data['servings']
+    recipe.description = data['description']
+    recipe.image_url = data['image_url']
+    
+    db.session.commit()
+    # serialization, transforming recipe back into JSON 
+    updated_recipe = {
+        'id': recipe.id,
+        'title': recipe.title,
+        'ingredients': recipe.ingredients,
+        'instructions': recipe.instructions,
+        'servings': recipe.servings,
+        'description': recipe.description,
+        'image_url': recipe.image_url
+    }
+    
+    return jsonify({'message': 'Recipe updated successfully', 'recipe': updated_recipe})
+
+# DELETE Route
+@app.route('/api/recipes/<int:recipe_id>', methods=['DELETE'])
+def delete_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    if not recipe:
+        return jsonify({'error': 'Recipe not found'}), 404
+    
+    db.session.delete(recipe)
+    db.session.commit()
+    return jsonify({'message': 'Recipe deleted successfully'})
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
