01fa8190a0cba5cb02a754bd26919f769b7b03d7
lee_c <gitleelee@gmail.com>
2024-01-06 14:04:28 -0800

    Updates to view full recipes.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import RecipeExcerpt from "./components/RecipeExerpt";
+import RecipeFull from "./components/RecipeFull";
 import "./App.css";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
 
   useEffect(() => {
     const fetchAllRecipes = async () => {
@@ -15,24 +17,35 @@ function App() {
           const data = await response.json();
           setRecipes(data);
         } else {
-          console.log("Oops - could not fetch recipes!");
+          console.log("Oops - could not fetch recipes!", "error");
         }
       } catch (e) {
         console.error("An error occurred during the request:", e);
-        console.log("An unexpected error occurred. Please try again later.");
+        console.log("An unexpected error occurre  d. Please try again later.", "error");
       }
     };
     fetchAllRecipes();
   }, []);
 
+  const handleSelectRecipe = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const handleUnselectRecipe = () => {
+    setSelectedRecipe(null);
+  };
+
   return (
     <div className='recipe-app'>
       <Header />
-      <div className='recipe-list'>
-        {recipes.map((recipe) => (
-          <RecipeExcerpt key={recipe.id} recipe={recipe} />
-        ))}
-      </div>
+      {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
+      {!selectedRecipe && (
+        <div className='recipe-list'>
+          {recipes.map((recipe) => (
+            <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
